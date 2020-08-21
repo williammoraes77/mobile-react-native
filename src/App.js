429717fe9a700f4d475d872d0a15406e1ed01515
William Moraes <william.morais@intergalaxy.io>
2020-08-21 14:01:34 -0300

    Fix: delete comments

--- src/App.js
+++ src/App.js
@@ -52,11 +52,6 @@ export default function App() {
             <Text style={styles.repository}>{repository.title}</Text>
 
             <View style={styles.techsContainer}>
-              {/* {repository.techs.map(tech => (
-                <Text key={techs} style={styles.tech}>
-                  {tech}
-                </Text>
-              ))} */}
               <Text style={styles.tech}>
                   {repository.techs}
               </Text>
@@ -84,37 +79,6 @@ export default function App() {
           </View>
           )}
         />
-        {/* <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View> */}
       </SafeAreaView>
     </>
   );
